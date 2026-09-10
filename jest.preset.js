6910024fc6dfa1d2c7c5357e3d8da444820e19c2
vladRiabichenko <vladyslav.riabichenko@devico.io>
2026-09-10 19:32:19 +0300

    fix: ci

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,10 @@
 const nxPreset = require('@nx/jest/preset').default;
 
-module.exports = { ...nxPreset, passWithNoTests: true };
+module.exports = {
+  ...nxPreset,
+  passWithNoTests: true,
+  // NestJS v12+ packages ship ESM; allow transforming them if present.
+  transformIgnorePatterns: [
+    'node_modules/(?!(.*\\.mjs$|@nestjs/config|@nestjs/passport|@nestjs/jwt))',
+  ],
+};
